bb1189a4fc1ad61dfa5ed10134330eb227407e32
yuchida0911 <yuta_uchida@rudel.jp>
2023-04-12 16:10:24 +0900

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { ProgressCircleScreen } from "./src/screens/ProgressCircleScreen";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, View } from "react-native";
 
-export default function App() {
+const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <ProgressCircleScreen />
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Button
+        title="Progress Circle"
+        onPress={() => navigation.navigate("ProgressCircleScreen")}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="ProgressCircleScreen"
+          component={ProgressCircleScreen}
+        />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
